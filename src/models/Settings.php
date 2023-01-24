86fcb0b1926dddd6154e50fb2407ab2c4b9f6cd3
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2023-01-24 13:57:11 +0000

    Update Settings

---
+++ src/models/Settings.php
@@ -0,0 +1,34 @@
+<?php
+namespace toward\towardsupport\models;
+
+use Craft;
+use craft\base\Model;
+
+class Settings extends Model
+{
+	// Public Properties
+	// =========================================================================
+
+	/**
+	 * @var string
+	 */
+
+	public $name = "Toward Support";
+	public $accountManager;
+	public $companyInfo = "We're in the studio 8:30 - 17:00 Monday to Friday. We aim to get back to you within 24 hours.";
+	public $supportUrl;
+
+	// Public Methods
+	// =========================================================================
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules(): array
+	{
+		return [
+			[['accountManager', 'companyInfo', 'supportUrl'], 'required'],
+			[['accountManager', 'companyInfo', 'supportUrl'], 'string'],
+		];
+	}
+}
